4f4e3456d6f298b1c99edcadda6f7ce4bd0e5a1a
manuel_ruelke <manuel.ruelke@sitewards.com>
2014-08-26 14:06:52 +0200

    added jslint to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -11,13 +11,43 @@ module.exports = function(grunt) {
                 src: 'src/loca.js',
                 dest: 'build/loca.min.js'
             }
+        },
+        jshint: {
+            src: [
+                /* 3rd party scripts are not included here */
+                'Gruntfile.js',
+                'src/loca.js',
+                'tests/impunit.js',
+                'tests/locadata.js',
+                'tests.js'
+            ],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                undef: true,
+                browser: true,
+                jquery: true,
+                '-W099': true, /* switching off "W099: Mixed spaces and tabs." */
+                globals: {
+                    /* specify missing globals here, if any */
+                    module: true,
+                    alert: true
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    // Load jshint
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'jshint']);
 
 };
